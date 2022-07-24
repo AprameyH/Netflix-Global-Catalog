9439cc42c42a662f67870f01ce87444a610905c5
Apramey <apramey.hosahalli@gmail.com>
2022-07-24 02:17:57 -0700

    Added update and delete users and enabled advanced query functionality with country dropdown

--- prod/app/database.py
+++ prod/app/database.py
@@ -2,7 +2,7 @@
 from app import db
 import random
 
-def fetch_movies() -> dict:
+def fetch_movies(country="All") -> dict:
     """Reads all tasks listed in the todo table
 
     Returns:
@@ -10,21 +10,103 @@ def fetch_movies() -> dict:
     """
 
     conn = db.connect()
-    query_results = conn.execute("SELECT movie_id, name, media_type, synopsis FROM Movie LIMIT 100;").fetchall()
+    if country == "All":
+        query_results = conn.execute("SELECT name, media_type, synopsis FROM Movie LIMIT 100;").fetchall()
+    else:
+        countryfilter = 'SELECT name, media_type, synopsis FROM (SELECT movie_id, country_name FROM Movie M NATURAL JOIN Availability A NATURAL JOIN Country C WHERE country_name = "{}") AS T NATURAL JOIN Movie M ORDER By name LIMIT 100'.format(country)
+        query_results = conn.execute(countryfilter)
+        
     conn.close()
     todo_list = []
     for result in query_results:
         item = {
-            "movie_id": result[0],
-            "name": result[1],
-            "media_type": result[2],
-            "synopsis": result[3]
+            "name": result[0],
+            "media_type": result[1],
+            "synopsis": result[2]
         }
         todo_list.append(item)
 
     return todo_list
 
 
+def fetch_countries():
+
+    conn = db.connect()
+    query_results = conn.execute('SELECT country_name FROM Country WHERE (country_name NOT LIKE "___") AND (country_name NOT LIKE "__") ORDER by country_name;').fetchall()
+    conn.close()
+    country_list = []
+    for result in query_results:
+        country_list.append(result[0])
+    
+    return country_list
+
+
+def search_movies(input, current_email, country="All") -> dict:
+    """Reads all tasks listed in the todo table
+
+    Returns:
+        A list of dictionaries
+    """
+    
+
+    conn = db.connect()
+
+    advanced_query2 = 'SELECT name, media_type, synopsis, country_name FROM (SELECT movie_id, country_name FROM Movie M NATURAL JOIN Availability A NATURAL JOIN Country C WHERE country_name = "{}") AS T NATURAL JOIN Movie M WHERE name LIKE "%%{}%%" ORDER By name;'.format(country, input)
+   
+    query_results = conn.execute(advanced_query2).fetchall()
+
+    conn.close()
+    todo_list = []
+    for result in query_results:
+        item = {
+            "name": result[0],
+            "media_type": result[1],
+            "synopsis": result[2]
+        }
+        todo_list.append(item)
+    if current_email != None:
+        conn = db.connect()
+        query_results = conn.execute('SELECT user_id FROM User WHERE email ="{}";'.format(current_email)).fetchall()
+        user_id = query_results[0][0]
+        movie_id = todo_list[0]["movie_id"]
+        search_insert ='INSERT INTO Search(user_id, search_text, movie_id) VALUES("{}", "{}", "{}")'.format(
+            user_id, input, movie_id)
+        conn.execute(search_insert)
+        conn.close()
+    
+    return todo_list
+
+
+def recommend_movies(current_email) -> dict:
+    # We first need the director name and the movie name of the most recently searched movie
+    conn = db.connect()
+    query_results = conn.execute(
+        'SELECT user_id FROM User WHERE email ="{}";'.format(current_email)).fetchall()
+    user_id = query_results[0][0]
+
+    #Get the most recently searched movie id
+    query = 'SELECT movie_id FROM Search WHERE user_id = {} ORDER by search_id DESC LIMIT 1;'.format(user_id)
+    query_results = conn.execute(query).fetchall()
+    recentsearchid = query_results[0][0]
+
+    # Get the movie name and director name
+    query = 'SELECT M.name, C.name FROM Movie M JOIN Cast C ON M.movie_id = C.movie_id WHERE M.movie_id = {} AND C.role = "Director"'.format(recentsearchid)
+    query_results = conn.execute(query).fetchall()
+    movie_name = query_results[0][0]
+    director_name = query_results[0][1]
+    
+    advanced_query_1 = '(SELECT M.name, M.media_type, M.synopsis FROM Movie M LEFT JOIN Cast C ON M.movie_id=C.movie_id WHERE C.role="Director" AND C.name = "{}") UNION (SELECT name, media_type, synopsis FROM Movie M NATURAL JOIN Genre G WHERE genre IN (SELECT genre FROM Movie M NATURAL JOIN Genre G WHERE name="{}"))'.format(director_name, movie_name)
+    query_results = conn.execute(advanced_query_1).fetchall()
+    conn.close()
+    rec_list = []
+    for result in query_results:
+        item = {
+            "name": result[0],
+            "media_type": result[1],
+            "synopsis": result[2]
+        }
+        rec_list.append(item)
+    return rec_list
 
 def update_task_entry(task_id: int, text: str) -> None:
     """Updates task description based on given `task_id`
@@ -83,9 +165,74 @@ def insert_new_movie(name: str, synop: str) ->  int:
     return task_id
 
 
+def insert_new_user(name: str, email: str) -> int:
+    """Insert new task to todo table.
+
+    Args:
+        text (str): Task description
+
+    Returns: The task ID for the inserted entry
+    """
+
+    conn = db.connect()
+    #query = 'Insert Into tasks (task, status) VALUES ("{}", "{}");'.format(
+    #    text, "Todo")
+    count_q = 'SELECT COUNT(*) FROM User WHERE email LIKE "{}";'.format(email)
+    count = (conn.execute(count_q).fetchall())
+    count = count[0][0]
+    
+
+    if count == 0:
+ 
+        query = 'INSERT INTO User (username, email) VALUES ("{}", "{}");'.format(
+        name, email)  # now need to change part of routes.py
+        conn.execute(query)
+
+
+    conn.close()
+
+    return 200
+
+
+def update_user(oldemail: str, name: str, email: str) -> int:
+    """Insert new task to todo table.
+
+    Args:
+        text (str): Task description
+
+    Returns: The task ID for the inserted entry
+    """
+
+    conn = db.connect()
+    #query = 'Insert Into tasks (task, status) VALUES ("{}", "{}");'.format(
+    #    text, "Todo")
+    query = 'UPDATE User SET username = "{}", email = "{}" WHERE email LIKE "{}";'.format(name, email, oldemail)
+    
+    conn.execute(query)
+
+
+    conn.close()
+
+    return 200
+
+def delete_user(username: str, email: str):
+
+    conn = db.connect()
+
+    query = 'DELETE FROM User WHERE username = "{}" AND email = "{}";'.format(username, email)
+
+    conn.execute(query)
+
+
+    conn.close()
+
+    return 200
+
+
 def remove_task_by_id(task_id: int) -> None:
     """ remove entries based on task ID """
     conn = db.connect()
     query = 'Delete From tasks where id={};'.format(task_id)
     conn.execute(query)
     conn.close()
+
--- prod/app/routes.py
+++ prod/app/routes.py
@@ -5,8 +5,9 @@ from app import app
 from app import database as db_helper
 
 #GLOBAL VARIABLES
-log_in = "False"
-username = "USER"
+log_in = False
+current_username = None
+current_email = None
 
 @app.route("/delete/<int:task_id>", methods=['POST'])
 def delete(task_id):
@@ -55,36 +56,85 @@ def create():
 
 @app.route("/", methods=['GET', 'POST'])
 def homepage():
-    """ returns rendered homepage """
+    global log_in
+    global current_email
+
     items = db_helper.fetch_movies()
-    return render_template("index.html", items=items)
+    clist2 = db_helper.fetch_countries()
+    
+    if request.method == 'POST':
+        selected_country = request.form["countrydropdown"]
+        items2 = db_helper.fetch_movies(selected_country)
+        input = request.form['searchBar']
+        if input != None:
+            print("Calling search movies")
+            items2 = db_helper.search_movies(input, current_email, selected_country)
+            print("Finished search movies")
+
+        return render_template("index.html", items=items2, clist = clist2)
+
+    return render_template("index.html", items=items, clist = clist2)
 
 @app.route("/account")
 def accountpage():
     global log_in
-
-    if log_in == "False":
+    global current_email
+    if log_in == False:
         return redirect("/login")
     
-    items = db_helper.fetch_movies()
-    return render_template("account.html", items=items, username = username)
+    items = db_helper.recommend_movies(current_email)
+    return render_template("account.html", items=items, username = current_username)
 
 @app.route("/login", methods=['GET', 'POST'])
 def loginpage():
     global log_in
-    global username
+    global current_username
+    global current_email
     """ displays login page and gets email """
     log_in = False
+    current_email = None
     if request.method =='POST':
-        if len(request.form['uemail']) != 0 and request.form['uemail'].find("@") != -1:
+        current_username = request.form['uname']
+        current_email = request.form['uemail']
+        
+        if len(current_email.strip()) != 0 and current_email.find("@") != -1 and len(current_username.strip()) != 0:
             
-            username = request.form['uname']
-            log_in = "True"
-            
+            log_in = True
+            db_helper.insert_new_user(current_username, current_email)
             return redirect("/account")
         else:
-            log_in = "False"
+            log_in = False
+            current_email = None
             return render_template("login.html", error="Please enter a valid email")
             
     return render_template("login.html", error="")
     
+
+@app.route("/update", methods=['GET', 'POST'])
+def updatepage():
+    global log_in
+    global current_username
+    global current_email
+    """ displays update page and gets email """
+    if request.method =='POST':
+        oldemail = request.form['oldemail']
+        newname = request.form['newname']
+        newemail = request.form['newemail']
+        if len(newname.strip()) != 0 and len(newemail.strip()) != 0 and newemail.find("@") != -1:
+            db_helper.update_user(oldemail, newname, newemail)
+            return redirect("/account")
+        
+        else:
+            return render_template("updateuser.html", error="Please enter a valid email")
+            
+    return render_template("updateuser.html", error="")
+
+
+@app.route("/delete", methods=['GET', 'POST','DELETE'])
+def deleteuser():
+    global current_email
+    global current_username
+
+    if current_email != None and current_username != None:
+        db_helper.delete_user(current_username, current_email)
+    return redirect("/login")
